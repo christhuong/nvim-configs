4b8ef4dd1a8e6161c9e5132782e90cebd38e9820
christhuong <thuong.huynh@employmenthero.com>
2024-08-11 19:29:50 +0700

    Update plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -17,7 +17,7 @@ Plug('saadparwaiz1/cmp_luasnip')
 -- syntax highlight
 Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
 -- search files
-Plug('junegunn/fzf', { ['do'] = vim.fn['fzf#install'] })
+Plug('junegunn/fzf', { ['do'] = vim.fn['fzf#install()'] })
 Plug('junegunn/fzf.vim')
 -- folder tree
 Plug('kyazdani42/nvim-tree.lua')
@@ -89,7 +89,7 @@ end
 
 local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local lspconfig = require('lspconfig')
-for _, server in pairs({ 'tsserver', 'solargraph' }) do
+for _, server in pairs({ 'tsserver' }) do
   lspconfig[server].setup({
     capabilities = lsp_capabilities,
     on_attach = lsp_on_attach,
@@ -162,25 +162,74 @@ require"nvim-treesitter.configs".setup {
 
 -- <-- start configuring git signs
 
-require"gitsigns".setup({
+-- require"gitsigns".setup({
+--   signs = {
+--     add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+--     change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+--     delete = { hl = "GitSignsDelete", text = "▎", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+--     topdelete = {
+--       hl = "GitSignsDelete",
+--       text = "契",
+--       numhl = "GitSignsDeleteNr",
+--       linehl = "GitSignsDeleteLn",
+--     },
+--     changedelete = {
+--       hl = "GitSignsChange",
+--       text = "▎",
+--       numhl = "GitSignsChangeNr",
+--       linehl = "GitSignsChangeLn",
+--     },
+--   },
+-- })
+require('gitsigns').setup {
   signs = {
-    add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-    change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    delete = { hl = "GitSignsDelete", text = "▎", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    topdelete = {
-      hl = "GitSignsDelete",
-      text = "契",
-      numhl = "GitSignsDeleteNr",
-      linehl = "GitSignsDeleteLn",
-    },
-    changedelete = {
-      hl = "GitSignsChange",
-      text = "▎",
-      numhl = "GitSignsChangeNr",
-      linehl = "GitSignsChangeLn",
-    },
+    add          = { text = '┃' },
+    change       = { text = '┃' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked    = { text = '┆' },
   },
-})
+  signs_staged = {
+    add          = { text = '┃' },
+    change       = { text = '┃' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked    = { text = '┆' },
+  },
+  signs_staged_enable = true,
+  signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+  numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+  linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+  word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+  watch_gitdir = {
+    follow_files = true
+  },
+  auto_attach = true,
+  attach_to_untracked = false,
+  current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+    delay = 1000,
+    ignore_whitespace = false,
+    virt_text_priority = 100,
+  },
+  current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+  sign_priority = 6,
+  update_debounce = 100,
+  status_formatter = nil, -- Use default
+  max_file_length = 40000, -- Disable if file is longer than this (in lines)
+  preview_config = {
+    -- Options passed to nvim_open_win
+    border = 'single',
+    style = 'minimal',
+    relative = 'cursor',
+    row = 0,
+    col = 1
+  },
+}
 
 -- end of configuring git signs -->
 
