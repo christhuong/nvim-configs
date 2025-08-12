9beaceb75e5a384e32069a950df4d4eb5605cb87
Thuong Huynh <hi.christhuong@gmail.com>
2025-08-12 21:26:08 +0700

    Update files

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -106,3 +106,4 @@ map('n', '<leader>cp', ':Copilot panel<CR>', default_opts)
 -- commands
 local create_user_command = vim.api.nvim_create_user_command
 create_user_command('Cdnvim', ':e ~/.config/nvim', {})
+create_user_command('CommitAllFiles', ':! git add . && git commit -m "Update files" && git push origin head', {})
